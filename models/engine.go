1b037906fd38135a657745e2c6b35626ef48b99c
P-Odiix <mahingarodin@gmail.com>
2025-11-19 10:03:40 +0200

    added validation functions in methods

--- models/engine.go
+++ models/engine.go
@@ -1,10 +1,54 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
 
 type Engine struct {
 	EngineID  uuid.UUID `json: "engine_id"`
 	Displacement int64 `json: "displacement"`
 	NoOfCyclinders int64 `json: "noOfCyclinders"`
 	CarRange int64 `json: "carRange"`
+}
+
+type EngineRequest struct {
+	EngineID  uuid.UUID `json: "engine_id"`
+	Displacement int64 `json: "displacement"`
+	NoOfCyclinders int64 `json: "noOfCyclinders"`
+	CarRange int64 `json: "carRange"`
+}
+
+func ValidateEngineRequest(engineReq EngineRequest) error {
+	if err := validateDisplacement(engineReq.Displacement); err != nil {
+		return err
+	}
+	if err := validateNoOfCyclinders(engineReq.NoOfCyclinders); err != nil {
+		return err
+	}
+	if err := validateCarRange(engineReq.CarRange); err != nil {
+		return err
+	}
+}
+
+func validateDisplacement(displacement int64) error {
+	if displacement <= 0 {
+		return errors.New("displacement must be greater than zero")
+	}
+	return nil
+}
+
+func validateNoOfCyclinders(noOfCyclinders int64) error {
+	if noOfCyclinders <= 0 {
+		return  errors.New("noOfCyclinders must be greater than zero")
+	}
+	return nil
+}
+
+func validateCarRange(carRange int64) error {
+	if carRange <= 0 {
+		return errors.New("Car range must be greater than zero")
+	}
+	return nil
 }
