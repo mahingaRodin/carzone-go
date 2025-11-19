8eeecf2f8ef264cbcb567c0b2559c82a1350dd02
P-Odiix <mahingarodin@gmail.com>
2025-11-19 10:28:53 +0200

    [created controller templates]

--- store/car/car.go
+++ store/car/car.go
@@ -1,1 +1,36 @@
 package car
+
+import (
+	"context"
+
+	"database/sql"
+
+	"github.com/mahingaRodin/carzone-go/models"
+)
+type Store struct {
+	db *sql.DB
+}
+
+func new(db *sql.DB) Store {
+	return Store{db: db}
+}
+
+func (s Store) GetCarById(ctx context.Context, id string) (models.Car) {
+
+}
+
+func (s Store) GetCarByBrand(ctx context.Context, brand string , isEngine bool) {
+
+}
+
+func (s Store) CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error) {
+
+}
+
+func (s Store) UpdateCar(ctx context.Context,id string , carReq *models.CarRequest) (models.Car, error) {
+
+}
+
+func (s Store) DeleteCar(ctx context.Context, id string) (models.Car, error) {
+
+}
--- store/engine/engine.go
+++ store/engine/engine.go
@@ -1,1 +1,34 @@
 package engine
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/mahingaRodin/carzone-go/models"
+)
+
+
+type EngineStore struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *EngineStore {
+	return &EngineStore{db: db}
+}
+
+func (e EngineStore) EngineById(ctx context.Context, id string) (models.Engine, error) {
+
+}
+
+func (e EngineStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine) {
+
+}
+
+func (e EngineStore) EngineUpdate(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error) {
+
+}
+
+func (e EngineStore) EngineDelete(ctx context.Context, id string) (models.Engine, error) {
+	
+}
+
